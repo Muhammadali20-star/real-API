f0deff047c1a5879fcb61329422548600e487a87
Muhammadali <muhamadalimamadaminov@gmail.com>
2025-06-14 12:12:45 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/login/Login'
-import Dashboard from './pages/dashboard/dashboard'
+import Dashboards from './pages/dashboard/Dashboards'
 import Register from './pages/register/Register'
 import Profile from './pages/dashboard/profile/Profile'
 import Layout from './pages/layout/Layout'
@@ -12,7 +12,7 @@ const App = () => {
     <>
       <Routes>
       <Route path='/' element={<Layout/>}>
-        <Route path="dashboard" element={<Dashboard />}>
+        <Route path="dashboard" element={<Dashboards />}>
          <Route path="" element={<Profile />} />
          <Route path="group/:id" element={<Groups />} />
         </Route>
